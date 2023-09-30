ab01ada83690c9dc6793c3b817ac5dd4feeb21d4
Dustin4D <DustinHolder14@yahoo.com>
2023-09-29 22:54:12 -0500

    Delete task

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ window.onload = () => {
 // Entry point
 const App = () => {
     console.log("Initializing app...");
-    attachEventListeners();
+    attachSubmitListener();
 }
 
 const currentTasks = [];
@@ -22,25 +22,42 @@ const addTask = (description) => {
     const Task = {
         id: currentTasks.length + 1,
         description: description,
+
+        delete: () => {
+            const index = currentTasks.findIndex(t => t.id === Task.id); // Run function for each array element, check against Task.id
+            if (index > -1) { // If the array isn't empty basically
+            currentTasks.splice(index, 1); // Start splice at the Task's index, `1` selects it
+            renderTasks(); // Update the list of tasks
+            }
+        },
+        edit: () => {
+
+        },
     }
     // Push this new task to the `currentTasks` array
     currentTasks.push(Task);
 }
 
-const attachEventListeners = () => {
+const attachSubmitListener = () => {
+    // Submit eventListener
     document.getElementById('createTask').addEventListener('submit', function(event){
     /* Prevent form from doing default behavior using an anonymous function that will use
     * the event's object as a parameter for the `.addEventListener()` method.
     */
     event.preventDefault();
 
-        const text = input.value;
-        if (text) {
-            addTask(text);
-            renderTasks();
-            input.value = null; // Clear the input field
-            }
+    const text = input.value.trim();
+    if (text) {
+        addTask(text);
+        renderTasks();
+        input.value = null; // Clear the input field
+        }
     })
+}
+
+const attachDeleteListener = () => {
+    //document.querySelector('btn-TaskDelete').addEventListener('click', this.delete());
+    console.log("Delete");
 }
 
 const renderTasks = () => {
@@ -61,9 +78,9 @@ const renderTasks = () => {
         // Create cell for delete button
         const btnDeleteTask = document.createElement('td');
         btnDeleteTask.innerHTML = '<i class="fa-solid fa-trash btn-TaskDelete"></i>';
+        btnDeleteTask.addEventListener('click', () => task.delete());
 
         // Iterate through array and append each array item to the `<tr>`
-        console.log(newTaskCell, btnEditTask, btnDeleteTask);
         [newTaskCell, btnEditTask, btnDeleteTask].forEach(cell => newTask.appendChild(cell));
 
         // Append the new `<tr>` to the table :)
